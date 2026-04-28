897cd6a4b84c1bae69c7d162c60cf6dfde0dc004
priyanka thakuri <thakuripriyanka95@gmail.com>
2026-04-28 19:25:51 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  return <LoginForm />;
+}
+
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography, Paper } from "@mui/material";
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    console.log({ email, password });
+    alert("Login Successful!");
+  };
+
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#f4f6f8",
+      }}
+    >
+      <Paper elevation={6} sx={{ padding: 4, width: 350 }}>
+        <Typography variant="h5" align="center" gutterBottom>
+          Login
+        </Typography>
+
+        <form onSubmit={handleLogin}>
+          <TextField
+            label="Email"
+            type="email"
+            fullWidth
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <TextField
+            label="Password"
+            type="password"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{ mt: 2 }}
+          >
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </Box>
+  );
+}
+
+export default LoginForm;
